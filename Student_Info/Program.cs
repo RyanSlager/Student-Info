39a3e31ee2d47d15bb0fe5f0935add77b2d7f3e5
danteCLRK <fryanslaucerers@gmail.com>
2017-07-05 19:13:25 -0400

    Student class is finished with Get/Set methods, StudentBuilder method is built

--- Student_Info/Program.cs
+++ Student_Info/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,42 @@ namespace Student_Info
     {
         static void Main(string[] args)
         {
+            Dictionary<int, Student> students = StudentBuilder();
+
+            Student stu = students[1];
+
+            int age = stu.GetAge();
+            string name = stu.GetName();
+            string food = stu.GetFood();
+            string home = stu.GetHome();
+
+            Console.WriteLine($"{name} is from {home}, and is {age}. Their favorite food is {food}");
+        }
+
+        public static Dictionary<int, Student> StudentBuilder()
+        {
+            Dictionary<int, Student> students = new Dictionary<int, Student>();
+            string c;
+            int cParsed;
+
+            Console.WriteLine("How many students would you like to create? \n");
+            c = Console.ReadLine();
+
+            while (!Int32.TryParse(c, out cParsed) || cParsed < 0)
+            {
+                Console.WriteLine("Please enter a positive integer: \n");
+                c = Console.ReadLine();
+            }
+
+
+            for (int i = 0; i < cParsed; i++)
+            {
+                Student student = new Student();
+                students.Add(i, student);
+            }
+            
+
+            return students;
         }
     }
 }
